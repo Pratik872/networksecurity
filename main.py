f2fac5f3196cfd7ad3902c106ae126b8e90c3104
Pratik Waghmare <waghmarepratik3@gmail.com>
2025-02-09 16:38:04 -0700

    model tariner files added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 
@@ -35,12 +36,12 @@ if __name__=='__main__':
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
-        #Model Training
-        # logging.info("Model Training sstared")
-        # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
-        # model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
-        # model_trainer_artifact=model_trainer.initiate_model_trainer()
-        # logging.info("Model Training artifact created")
+        # Model Training
+        logging.info("Model Training started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training artifact created")
         
         
     except Exception as e:
